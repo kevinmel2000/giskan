4fdefc07bf450fab3388d0174f47821e1c632db4
axver <ecimoramora@gmail.com>
2018-04-14 18:25:21 +0700

    Yoga Homo

--- index.php
+++ index.php
@@ -153,33 +153,7 @@
             <div class="content">
                 <div class="container-fluid">
 
-                  <div class="row" style="background-color:#c2f276">
 
-                    <div class="panel panel-info">
-
-                      <div class="panel-body">
-                        <?php
-
-                        //Load data auto gempa
-
-                        $list_warning= new SimpleXMLElement('http://data.bmkg.go.id/Maritim_Cuaca_Pelayaran.xml',null,true);
-                        $data=$list_warning->Laut;
-                        $total= $data->count();
-                        $i=0;
-                        while ($i<$total)
-                        {
-                          echo $data."<br/>";
-                          $i++;
-                        }
-                  ?>
-
-                      </div>
-
-                    </div>
-
-
-
-                  </div>
                     <div class="row">
                         <div class="col-md-12">
                           <!-- Div Peta -->
@@ -218,19 +192,74 @@
                                   $total_gelombang=$gelombang->count();
                                   // echo $total_gelombang;
 
-                                  $i=0;
-                                  while ($i<$total)
-                                  {
-                                    echo $data->Wil[$i]."<br/>";
-                                    $i++;
-                                  }
+
                             ?>
 
+                            <div class="card strpied-tabled-with-hover">
+                                <div class="card-header ">
+                                    <h4 class="card-title">Striped Table with Hover</h4>
+                                    <p class="card-category">Here is a subtitle for this table</p>
+                                </div>
+                                <div class="card-body table-full-width table-responsive">
+                                    <table class="table table-hover table-striped">
+                                        <thead>
+                                            <th>No</th>
+                                            <th>Daerah</th>
+                                        </thead>
+                                        <tbody>
+                                            <?php
 
+                                            $i=0;
+                                            $j=$i+1;
+                                            while ($i<$total)
+                                            {
+                                              echo "<tr>"."<td>".$j."</td>"."<td>".$data->Wil[$i]."</td>"."</tr>";
+                                              $i++;
+                                              $j++;
+                                            }
+
+                                             ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
+
                         <div class="col-md-6">
+                          <h2> Info Gelombang </h2>
+                          <?php
 
+                          //Load data auto gempa
+
+                          $list_warning= new SimpleXMLElement('http://data.bmkg.go.id/Maritim_Cuaca_Pelayaran.xml',null,true);
+                          $data=$list_warning->Laut;
+                          $total= $data->count();
+                          $i=0;
+                          while ($i<$total)
+                          {
+                            echo $data."<br/>";
+                            $i++;
+                          }
+                    ?>
                         </div>
+
+
+
+
+                            <div class="panel panel-info">
+
+                              <div class="panel-body">
+
+
+                              </div>
+
+                            </div>
+
+
+
+
+
+
                     </div>
                 </div>
             </div>
